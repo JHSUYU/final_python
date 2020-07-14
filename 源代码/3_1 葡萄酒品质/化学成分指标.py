b176be33727bb6720231a29207bd172cd433a3e5
Condor <18921873921@163.com>
2020-07-14 18:40:30 +0800

    cyh: 文件添加

--- 源代码/3_1 葡萄酒品质/化学成分指标.py
+++ 源代码/3_1 葡萄酒品质/化学成分指标.py
@@ -32,87 +32,80 @@ r = dfw.columns.tolist()
 r.insert(0, r.pop())
 dfw = dfw.reindex(columns=r)
 
-#氯化物浓度对评分影响
-plt.figure(figsize = (10,4))
-plt.suptitle('氯化物浓度对评分的影响', y=1.02, fontsize = 16) #总标题
+# 氯化物浓度对评分影响
+plt.suptitle('氯化物浓度对评分的影响', y=1.00, fontsize=16, fontproperties=my_font)  # 总标题
 """红"""
-plt.subplot(1,2,1)
+plt.subplot(1, 2, 1)
 sns.boxplot(x=dfr['quality'], y=dfr['chlorides'])
-plt.xlabel('红葡萄酒评分',fontsize = 12)
-plt.ylabel('氯化物浓度',fontsize = 12)
+plt.xlabel('红葡萄酒评分', fontsize=12, fontproperties=my_font)
+plt.ylabel('氯化物浓度', fontsize=12, fontproperties=my_font)
 """白"""
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 sns.boxplot(x=dfw['quality'], y=dfw['chlorides'])
-plt.xlabel('白葡萄酒评分',fontsize = 12)
-plt.ylabel('氯化物浓度',fontsize = 12)
+plt.xlabel('白葡萄酒评分', fontsize=12, fontproperties=my_font)
+plt.ylabel('氯化物浓度', fontsize=12, fontproperties=my_font)
 plt.show()
 
-#硫酸盐浓度对评分影响
-plt.figure(figsize = (10,4))
-plt.suptitle('硫酸盐浓度对评分的影响', y=1.0, fontsize = 16) #总标题
+# 硫酸盐浓度对评分影响
+plt.suptitle('硫酸盐浓度对评分的影响', y=1.0, fontsize=16, fontproperties=my_font)  # 总标题
 """红"""
-plt.subplot(1,2,1)
+plt.subplot(1, 2, 1)
 sns.boxplot(x=dfr['quality'], y=dfr['sulphates'])
-plt.xlabel('红葡萄酒评分',fontsize = 12)
-plt.ylabel('硫酸盐浓度',fontsize = 12)
+plt.xlabel('红葡萄酒评分', fontsize=12, fontproperties=my_font)
+plt.ylabel('硫酸盐浓度', fontsize=12, fontproperties=my_font)
 """白"""
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 sns.boxplot(x=dfw['quality'], y=dfw['sulphates'])
-plt.xlabel('白葡萄酒评分',fontsize = 12)
-plt.ylabel('硫酸盐浓度',fontsize = 12)
+plt.xlabel('白葡萄酒评分', fontsize=12, fontproperties=my_font)
+plt.ylabel('硫酸盐浓度', fontsize=12, fontproperties=my_font)
 plt.show()
 
-#游离二氧化硫占总二氧化硫比重
-plt.figure(figsize = (10,4))
-plt.suptitle('游离二氧化硫占总二氧化硫比重分布情况', y=1.0, fontsize = 16) #总标题
+# 游离二氧化硫占总二氧化硫比重
+plt.suptitle('游离二氧化硫占总二氧化硫比重分布情况', y=1.0, fontsize=16, fontproperties=my_font)  # 总标题
 """红"""
-plt.subplot(1,2,1)
-temp = dfr[{'free sulfur dioxide','total sulfur dioxide'}]
-temp['precent'] = temp.apply(lambda x: x['free sulfur dioxide']/x['total sulfur dioxide'], axis=1) #计算占比
-temp['precent'].hist(bins = 100, color = color[0])
-plt.xlabel('红葡萄酒游离二氧化硫占比',fontsize = 12)
-plt.ylabel('频数',fontsize = 12)
+plt.subplot(1, 2, 1)
+temp = dfr[{'free sulfur dioxide', 'total sulfur dioxide'}]
+temp['precent'] = temp.apply(lambda x: x['free sulfur dioxide'] / x['total sulfur dioxide'], axis=1)  # 计算占比
+temp['precent'].hist(bins=100, color=color[0])
+plt.xlabel('红葡萄酒游离二氧化硫占比', fontsize=12, fontproperties=my_font)
+plt.ylabel('频数', fontsize=12, fontproperties=my_font)
 """白"""
-plt.subplot(1,2,2)
-temp = dfw[{'free sulfur dioxide','total sulfur dioxide'}]
-temp['precent'] = temp.apply(lambda x: x['free sulfur dioxide']/x['total sulfur dioxide'], axis=1) #计算占比
-temp['precent'].hist(bins = 100, color = color[0])
-plt.xlabel('白葡萄酒游离二氧化硫占比',fontsize = 12)
-plt.ylabel('频数',fontsize = 12)
+plt.subplot(1, 2, 2)
+temp = dfw[{'free sulfur dioxide', 'total sulfur dioxide'}]
+temp['precent'] = temp.apply(lambda x: x['free sulfur dioxide'] / x['total sulfur dioxide'], axis=1)  # 计算占比
+temp['precent'].hist(bins=100, color=color[0])
+plt.xlabel('白葡萄酒游离二氧化硫占比', fontsize=12, fontproperties=my_font)
+plt.ylabel('频数', fontsize=12, fontproperties=my_font)
 plt.show()
 
-#游离二氧化硫占比对评分影响
-plt.figure(figsize = (10,4))
-plt.suptitle('游离二氧化硫占总二氧化硫比重对评分的影响', y=1.0, fontsize = 16) #总标题
+# 游离二氧化硫占比对评分影响
+plt.suptitle('游离二氧化硫占总二氧化硫比重对评分的影响', y=1.0, fontsize=16, fontproperties=my_font)  # 总标题
 """红"""
-plt.subplot(1,2,1)
-temp = dfr[{'free sulfur dioxide','total sulfur dioxide','quality'}]
-temp['precent'] = temp.apply(lambda x: x['free sulfur dioxide']/x['total sulfur dioxide'], axis=1)
+plt.subplot(1, 2, 1)
+temp = dfr[{'free sulfur dioxide', 'total sulfur dioxide', 'quality'}]
+temp['precent'] = temp.apply(lambda x: x['free sulfur dioxide'] / x['total sulfur dioxide'], axis=1)
 sns.boxplot(x=temp['quality'], y=temp['precent'])
-plt.xlabel('红葡萄酒评分',fontsize = 12)
-plt.ylabel('游离二氧化硫占比',fontsize = 12)
+plt.xlabel('红葡萄酒评分', fontsize=12, fontproperties=my_font)
+plt.ylabel('游离二氧化硫占比', fontsize=12, fontproperties=my_font)
 """白"""
-plt.subplot(1,2,2)
-temp = dfw[{'free sulfur dioxide','total sulfur dioxide','quality'}]
-temp['precent'] = temp.apply(lambda x: x['free sulfur dioxide']/x['total sulfur dioxide'], axis=1)
+plt.subplot(1, 2, 2)
+temp = dfw[{'free sulfur dioxide', 'total sulfur dioxide', 'quality'}]
+temp['precent'] = temp.apply(lambda x: x['free sulfur dioxide'] / x['total sulfur dioxide'], axis=1)
 sns.boxplot(x=temp['quality'], y=temp['precent'])
-plt.xlabel('白葡萄酒评分',fontsize = 12)
-plt.ylabel('游离二氧化硫占比',fontsize = 12)
+plt.xlabel('白葡萄酒评分', fontsize=12, fontproperties=my_font)
+plt.ylabel('游离二氧化硫占比', fontsize=12, fontproperties=my_font)
 plt.show()
 
-#二氧化硫总量对评分影响
-plt.figure(figsize = (10,4))
-plt.suptitle('二氧化硫总量对评分的影响', y=1.0, fontsize = 16) #总标题
+# 二氧化硫总量对评分影响
+plt.suptitle('二氧化硫总量对评分的影响', y=1.0, fontsize=16, fontproperties=my_font)  # 总标题
 """红"""
-plt.subplot(1,2,1)
+plt.subplot(1, 2, 1)
 sns.boxplot(x=dfr['quality'], y=dfr['total sulfur dioxide'])
-plt.xlabel('红葡萄酒评分',fontsize = 12)
-plt.ylabel('二氧化硫总量',fontsize = 12)
+plt.xlabel('红葡萄酒评分', fontsize=12, fontproperties=my_font)
+plt.ylabel('二氧化硫总量', fontsize=12, fontproperties=my_font)
 """白"""
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 sns.boxplot(x=dfw['quality'], y=dfw['total sulfur dioxide'])
-plt.xlabel('白葡萄酒评分',fontsize = 12)
-plt.ylabel('二氧化硫总量',fontsize = 12)
+plt.xlabel('白葡萄酒评分', fontsize=12, fontproperties=my_font)
+plt.ylabel('二氧化硫总量', fontsize=12, fontproperties=my_font)
 plt.show()
-
-
